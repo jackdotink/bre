a5419ee8d09c6d513cbc13f810c30133ce825978
jack.ink <jackdotink@gmail.com>
2025-05-27 13:52:46 -0500

    clean up main

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,4 @@ mod cli;
 
 fn main() {
     cli::cli();
-
-    // let compiler = luau::Compiler::default();
-    // let bytecode = compiler.compile(std::fs::read_to_string("main.luau").unwrap().as_str());
-
-    // let executor = runtime::Executor::default();
-    // let luau = luau::Luau::new(executor.spawner());
-
-    // luau.execute(c"main", &bytecode);
-    // executor.run();
 }
