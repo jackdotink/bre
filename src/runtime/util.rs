bd199242405b9f3f65ba09809b90fc48820b6b2f
jack.ink <jackdotink@gmail.com>
2025-05-29 18:06:24 -0500

    fix unblock

--- src/runtime/util.rs
+++ src/runtime/util.rs
@@ -1,22 +1,27 @@
-use std::{
-    sync::{Arc, Mutex},
-    task::{Poll, Waker},
-};
+use std::{cell::RefCell, task::Poll};
 
-use crossbeam::channel::{self, Receiver};
+use crossbeam::channel::{self, Receiver, Sender};
 
-pub struct UnblockFuture<T: Send> {
+pub struct UnblockFuture<F: FnOnce() -> T + Send + 'static, T: Send + 'static> {
+    tx: Sender<T>,
     rx: Receiver<T>,
-    waker: Arc<Mutex<Option<Waker>>>,
+    func: RefCell<Option<F>>,
 }
 
-impl<T: Send> Future for UnblockFuture<T> {
+impl<F: FnOnce() -> T + Send + 'static, T: Send + 'static> Future for UnblockFuture<F, T> {
     type Output = T;
 
     fn poll(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Self::Output> {
         if self.rx.is_empty() {
-            let mut waker_slot = self.waker.lock().unwrap();
-            *waker_slot = Some(cx.waker().clone());
+            let tx = self.tx.clone();
+            let func = self.func.take().unwrap();
+            let waker = cx.waker().clone();
+            rayon::spawn(move || {
+                let result = func();
+                tx.send(result).unwrap();
+
+                waker.wake_by_ref();
+            });
 
             Poll::Pending
         } else {
@@ -26,24 +31,13 @@ impl<T: Send> Future for UnblockFuture<T> {
     }
 }
 
-pub fn unblock<F, T>(func: F) -> UnblockFuture<T>
+pub fn unblock<F, T>(func: F) -> UnblockFuture<F, T>
 where
     F: FnOnce() -> T + Send + 'static,
     T: Send + 'static,
 {
     let (tx, rx) = channel::bounded(1);
-    let waker = Arc::new(Mutex::new(None::<Waker>));
+    let func = RefCell::new(Some(func));
 
-    let thread_waker = waker.clone();
-    rayon::spawn(move || {
-        let result = func();
-        tx.send(result).unwrap();
-
-        let waker = thread_waker.lock().unwrap();
-        if let Some(waker) = &*waker {
-            waker.wake_by_ref();
-        }
-    });
-
-    UnblockFuture { rx, waker }
+    UnblockFuture { tx, rx, func }
 }
